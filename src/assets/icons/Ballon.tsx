cd99ca2594682b31cba894a3febca664544da203
ratoyan <ratoyan2002@gmail.com>
2026-03-24 22:02:55 +0400

    commit

---
+++ src/assets/icons/Ballon.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import Svg, {Ellipse, Path} from "react-native-svg"
+
+function Ballon(props: any) {
+    return (
+        <Svg
+            width={154}
+            height={156}
+            viewBox="0 0 64 96"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <Ellipse
+                cx={32}
+                cy={40}
+                rx={24}
+                ry={32}
+                fill={props.color ?? '#FF4B4B'}
+                stroke={props.color ?? '#FF4B4B'}
+                strokeWidth={2}
+            />
+            <Ellipse cx={24} cy={28} rx={6} ry={10} fill="#fff" opacity={0.4}/>
+            <Path
+                stroke="#fff"
+                strokeWidth={1}
+                d="
+                    M32 72
+                    L30 76
+                    L34 80
+                    L30 84
+                    L34 88
+                    L32 206
+                  "
+            /> </Svg>
+    )
+}
+
+export default Ballon
--- src/components/ui/Play/PlayBox.tsx
+++ src/components/ui/Play/PlayBox.tsx
@@ -3,6 +3,7 @@ import {Pressable} from "react-native";
 
 // icons
 import Card1 from "../../../assets/icons/Card1.tsx";
+import Ballon from "../../../assets/icons/Ballon.tsx";
 
 interface PlayBoxProps {
     box: any;
@@ -11,35 +12,36 @@ interface PlayBoxProps {
 
 function PlayBox({box, handlePress}: PlayBoxProps) {
     return (
-        <Pressable
-            onPress={handlePress}
-            style={{
-                position: "absolute", // important for translate to work
-                width: box.size[0],
-                height: box.size[1],
-                backgroundColor: box.color || "blue", // optional color
-                transform: [
-                    {translateX: box.x + box.size[0] / 2},
-                    {translateY: box.y + box.size[1] / 2},
-                    {rotate: `${box.rotation}deg`}, // rotation in degrees
-                    {translateX: -box.size[0] / 2},
-                    {translateY: -box.size[1] / 2},
-                ],
-            }}
-        />
-        // <Pressable onPress={handlePress}
-        //            style={{
-        //                position: "absolute", // important for translate to work
-        //                transform: [
-        //                    {translateX: box.x + box.size[0] / 2},
-        //                    {translateY: box.y + box.size[1] / 2},
-        //                    {rotate: `${box.rotation + 30}deg`}, // rotation in degrees
-        //                    {translateX: -box.size[0] / 2},
-        //                    {translateY: -box.size[1] / 2},
-        //                ],
-        //            }}>
-        //     <Card1 width={80} height={80}/>
-        // </Pressable>
+        // <Pressable
+        //     onPress={handlePress}
+        //     style={{
+        //         position: "absolute", // important for translate to work
+        //         width: box.size[0],
+        //         height: box.size[1],
+        //         backgroundColor: box.color || "blue", // optional color
+        //         transform: [
+        //             {translateX: box.x + box.size[0] / 2},
+        //             {translateY: box.y + box.size[1] / 2},
+        //             {rotate: `${box.rotation}deg`}, // rotation in degrees
+        //             {translateX: -box.size[0] / 2},
+        //             {translateY: -box.size[1] / 2},
+        //         ],
+        //     }}
+        // />
+        <Pressable onPress={handlePress}
+                   style={{
+                       position: "absolute", // important for translate to work
+                       transform: [
+                           {translateX: box.x + box.size[0] / 2},
+                           {translateY: box.y + box.size[1] / 2},
+                           {rotate: `${box.rotation + 30}deg`}, // rotation in degrees
+                           {translateX: -box.size[0] / 2},
+                           {translateY: -box.size[1] / 2},
+                       ],
+                   }}>
+            {/*<Card1 width={80} height={80}/>*/}
+            <Ballon color={box.color}/>
+        </Pressable>
     )
 }
 

21b030b50d6525170915e737e337b11b60d86f1e
ratoyan <ratoyan2002@gmail.com>
2026-03-31 22:07:50 +0400

    play

--- src/data/play.ts
+++ src/data/play.ts
@@ -17,17 +17,17 @@ import BackgroundImage from '../assets/images/background1.jpg';
 // ];
 
 export const boxes: BoxType[] = [
-    {id: 1, size: [80, 80], color: 'red', rotation: 130, x: 100, y: 0},
-    {id: 2, size: [80, 80], color: 'red', rotation: 130, x: 100, y: 0},
-    {id: 3, size: [80, 80], color: 'blue', rotation: 0, x: 200, y: 0},
-    {id: 4, size: [80, 80], color: 'blue', rotation: 0, x: 200, y: 0},
-    {id: 5, size: [80, 80], color: 'green', rotation: 60, x: 250, y: 0},
-    {id: 6, size: [80, 80], color: 'green', rotation: 60, x: 250, y: 0},
-    {id: 7, size: [80, 80], color: 'yellow', rotation: 120, x: 300, y: 0},
-    {id: 8, size: [80, 80], color: 'yellow', rotation: 120, x: 300, y: 0},
-    {id: 9, size: [80, 80], color: 'orange', rotation: 80, x: 400, y: 0},
-    {id: 10, size: [80, 80], color: 'purple', rotation: 0, x: 50, y: 0},
-    {id: 11, size: [80, 80], color: 'purple', rotation: 0, x: 50, y: 0},
+    {id: 1, size: [154, 156], color: 'red', rotation: 130, x: 100, y: 0},
+    {id: 2, size: [154, 156], color: 'red', rotation: 130, x: 100, y: 0},
+    {id: 3, size: [154, 156], color: 'blue', rotation: 0, x: 200, y: 0},
+    {id: 4, size: [154, 156], color: 'blue', rotation: 0, x: 200, y: 0},
+    {id: 5, size: [154, 156], color: 'green', rotation: 60, x: 250, y: 0},
+    {id: 6, size: [154, 156], color: 'green', rotation: 60, x: 250, y: 0},
+    {id: 7, size: [154, 156], color: 'yellow', rotation: 120, x: 300, y: 0},
+    {id: 8, size: [154, 156], color: 'yellow', rotation: 120, x: 300, y: 0},
+    {id: 9, size: [154, 156], color: 'orange', rotation: 80, x: 400, y: 0},
+    {id: 10, size: [154, 156], color: 'purple', rotation: 0, x: 50, y: 0},
+    {id: 11, size: [154, 156], color: 'purple', rotation: 0, x: 50, y: 0},
 ];
 
 export const colors: string[] = [
--- src/screens/Play/Play.tsx
+++ src/screens/Play/Play.tsx
@@ -220,7 +220,7 @@ export default function Play() {
                         });
                     }
 
-                    const newTx = Math.abs(dx) < 1 ? Math.random() * (width - b.size[0]) : b.tx;
+                    const newTx = Math.abs(dx) < 1 ? Math.random() * (width - b.size[0] + 70) : b.tx;
                     const newTy = b.y + duration;
                     const newRotation = (b.rotation + 2) % 360; // 2 degrees per frame
 

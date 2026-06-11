ff3babb895e5f98ffc7711ced0d13f57083b4857
ratoyan <ratoyan2002@gmail.com>
2026-06-12 00:31:01 +0400

    commit

--- src/services/config.ts
+++ src/services/config.ts
@@ -6,5 +6,5 @@ import { Platform } from 'react-native';
 const DEV_HOST = Platform.OS === 'android' ? '10.0.2.2' : 'localhost';
 
 export const API_BASE_URL = __DEV__
-    ? `http://${DEV_HOST}:3000/api`
+    ? `http://10.0.2.2:8000/api`
     : 'https://your-production-domain.com/api'; // ← replace before release

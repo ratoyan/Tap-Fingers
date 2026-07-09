148135a5eb95340e8b59cc55237fa3f455d97d89
ratoyan <ratoyan2002@gmail.com>
2026-07-09 23:37:48 +0400

    commit

--- android/app/src/main/java/com/webixworld/tapfingers/MainActivity.kt
+++ android/app/src/main/java/com/webixworld/tapfingers/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.webixworld.tapfingers
 
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // Must run before super.onCreate() so the launch theme is swapped for AppTheme.
+    installSplashScreen()
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule

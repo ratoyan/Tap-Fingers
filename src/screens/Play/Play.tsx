293aee2ea999caec194a3570669190b4c22f5267
ratoyan <ratoyan2002@gmail.com>
2026-07-12 23:58:12 +0400

    commit

--- src/screens/Play/Play.tsx
+++ src/screens/Play/Play.tsx
@@ -282,7 +282,7 @@ export default function Play() {
         maxComboRef.current = 0;
         sessionStartRef.current = Date.now();
         sessionTokenRef.current = null;
-        setLevelLength(2);
+        setLevelLength(30);
 
         try {
             // The token returned here is what lets submitGameSession() report the

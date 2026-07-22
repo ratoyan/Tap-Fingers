d60d4428e86286714ee5a692f34c714b5b57c81c
ratoyan <ratoyan2002@gmail.com>
2026-07-22 23:05:15 +0400

    commit

--- src/components/ui/MenuButton/MenuButton.tsx
+++ src/components/ui/MenuButton/MenuButton.tsx
@@ -3,6 +3,8 @@ import {Animated, Text, TouchableOpacity, View} from "react-native";
 import {MenuType} from "../../../types/menu.type.ts";
 import {useTranslation} from "react-i18next";
 import {useNavigation} from "@react-navigation/core";
+import {haptic} from "../../../utils/haptics.ts";
+import {playSfxVaried} from "../../../utils/sfx.ts";
 
 // icons
 import {MENU_ICONS} from "../../../assets/icons/MenuIcons.tsx";
@@ -21,6 +23,15 @@ function MenuButton({menu}: MenuProps){
     const {t} = useTranslation();
     const navigation = useNavigation();
     const scale = useRef(new Animated.Value(1)).current;
+
+    // Feedback for a page switch from the menu: a light buzz plus the browsing
+    // sound (pitch-varied so repeated taps don't hit the same note).
+    const handleNavigate = () => {
+        haptic('equip');
+        playSfxVaried('equip');
+        // @ts-ignore
+        navigation.navigate(menu.navigateTo);
+    };
 
     const handlePressIn = () => {
         Animated.spring(scale, {
@@ -42,8 +53,7 @@ function MenuButton({menu}: MenuProps){
         <Animated.View style={{transform: [{scale}]}}>
             <TouchableOpacity
                 activeOpacity={0.8}
-                // @ts-ignore
-                onPress={() => navigation.navigate(menu.navigateTo)}
+                onPress={handleNavigate}
                 onPressIn={handlePressIn}
                 onPressOut={handlePressOut}
                 style={styles.buttonContainer}
